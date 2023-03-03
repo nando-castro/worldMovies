acffaadbd3b901b2db049e3eb9fd0e8a31ceaded
Fernando <fernando.castro10.master@gmail.com>
2023-03-03 20:25:43 -0300

    feat: create pages movie and search

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import ResetCSS from "./assets/css/reset";
 import GlobalStyle from "./assets/css/global";
-import { Home } from "./pages/Home";
+import Home from "./pages/Home";
+import Movie from "./pages/Movie";
+import Search from "./pages/Search";
 
 function App() {
   return (
@@ -10,6 +12,8 @@ function App() {
       <GlobalStyle />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="movie/:id" element={<Movie />} />
+        <Route path="search" element={<Search />} />
       </Routes>
     </BrowserRouter>
   );
