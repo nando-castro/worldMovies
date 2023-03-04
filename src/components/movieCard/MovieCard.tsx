20921eeb8169ef13a88f9108e8df5537d6117336
Fernando <fernando.castro10.master@gmail.com>
2023-03-04 11:04:44 -0300

    feat: create components card and render results

---
+++ src/components/movieCard/MovieCard.tsx
@@ -0,0 +1,75 @@
+import styled from "styled-components";
+
+import { FaStar } from "react-icons/fa";
+import { Link } from "react-router-dom";
+
+const imageUrl = import.meta.env.VITE_IMG;
+
+const MovieCard = ({
+  movie,
+  showLink = true,
+}: {
+  movie: any;
+  showLink: boolean;
+}) => {
+  return (
+    <Container>
+      <Vote>
+        <FaStar /> {movie.vote_average}
+      </Vote>
+      <img src={imageUrl + movie.poster_path} alt={movie.title} />
+      <Title>{movie.title}</Title>
+      <Info>
+        {showLink && <Link to={`/movie/${movie.id}`}>Detalhes</Link>}
+      </Info>
+    </Container>
+  );
+};
+
+const Container = styled.main`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  border: 1px solid #000;
+
+  img {
+    width: 100%;
+  }
+
+
+`;
+
+const Title = styled.h2`
+  height: 100px;
+  font-size: 2.1rem;
+  padding-top: 1rem;
+  padding-left: 1rem;
+  line-height: 2rem;
+`;
+
+const Info = styled.div`
+    padding: 1rem;
+    font-size: 25px;
+    text-align: center;
+    color: #000;
+    border-radius: 50px 0 50px 0;
+    background-color: gold;
+    transition: 0.4s;
+    cursor: pointer;
+`;
+
+const Vote = styled.p`
+  position: absolute;
+  margin-top: 5px;
+  margin-left: 5px;
+  z-index: 1;
+  font-size: 30px;
+  color: gold;
+  border-radius: 10px;
+  border: 1px solid #000;
+  padding: 5px;
+  background-color: #FFF;
+`;
+
+export default MovieCard;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,14 +1,48 @@
+import { useEffect, useState } from "react";
 import styled from "styled-components";
+import MovieCard from "../components/movieCard/MovieCard";
 import NavBar from "../components/navbar/NavBar";
+import { api } from "../services/api";
+
+const moviesURL = import.meta.env.VITE_API;
+const apiKey = import.meta.env.VITE_API_KEY;
 
 const Home = () => {
+  const [topMovies, setTopMovies] = useState([]);
+
+  const getTopRatedMovies = async (url: string) => {
+    await api
+      .get(url)
+      .then((res) => {
+        setTopMovies(res.data.results);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  useEffect(() => {
+    const topRatedUrl = `top_rated?${apiKey}`;
+
+    getTopRatedMovies(topRatedUrl);
+  }, []);
+
+  const renderMoviesCard = () => {
+    return topMovies.map((movie: any) => (
+      <Content>
+        <MovieCard key={movie.id} movie={movie} showLink />
+      </Content>
+    ));
+  };
+
   return (
     <Container>
       <Top>
         <NavBar />
       </Top>
       <Body>
-        Corpo
+        <SubTitle>Melhores Filmes:</SubTitle>
+        {topMovies.length > 0 ? renderMoviesCard() : <p>Carregando...</p>}
       </Body>
     </Container>
   );
@@ -22,12 +56,32 @@ const Container = styled.main`
 const Top = styled.div`
   width: 100%;
   height: 60px;
-  background-color: red;
 `;
 
-const Body = styled.main`
+const Body = styled.div`
   width: 100%;
-  height: 100%;
+  display: flex;
+  flex-wrap: wrap;
+  padding: 2rem;
+  margin: 0 auto;
+`;
+
+const SubTitle = styled.h2`
+  width: 100%;
+  height: 50px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-size: 32px;
+  font-weight: bold;
+  color: gold;
+`;
+
+const Content = styled.div`
+  width: 400px;
+  max-height: 754px;
+
+  margin: 2rem;
 `;
 
 export default Home;
